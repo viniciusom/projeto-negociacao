dcdada42d874dcc5fc20a38e8cf6e22fb50c6a51
Vinicius Moraes <viniciusom01@hotmail.com>
2020-01-21 18:07:03 -0300

    Adicionado classes de Negociação

---
+++ js/app/controllers/NegociacaoController.js
@@ -0,0 +1,24 @@
+class NegociacaoController {
+
+    constructor(){
+        let $ = document.querySelector.bind(document);
+        
+        this._inputData = $("#data");
+        this._inputQuantidade = $("#quantidade");
+        this._inputValor = $("#valor");
+    }
+
+    adiciona(event) {
+        event.preventDefault();
+        
+        console.log(typeof(this._inputData.value));
+
+        console.log(this._inputData.value);
+
+        let data = new Date(this._inputData.value.replace(/-/g, ','));
+        console.log(data);
+
+    }
+
+
+}

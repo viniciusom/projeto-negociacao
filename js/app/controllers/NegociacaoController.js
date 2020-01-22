de0e598ec03ee95ea8af18b3562eb8ecce24d41f
Vinicius Moraes <viniciusom01@hotmail.com>
2020-01-22 10:05:54 -0300

    Finalizado a classe controller

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -11,14 +11,17 @@ class NegociacaoController {
     adiciona(event) {
         event.preventDefault();
         
-        console.log(typeof(this._inputData.value));
-
-        console.log(this._inputData.value);
-
-        let data = new Date(this._inputData.value.replace(/-/g, ','));
-        console.log(data);
+        let data = new Date(...
+            this._inputData.value.split('-')
+            .map((item, indice) => item - indice % 2)
+            );
+        
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
 
     }
-
 
 }

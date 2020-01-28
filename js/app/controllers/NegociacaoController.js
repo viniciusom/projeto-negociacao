80e42d35431e3204ae3e4a9971b0259d3c0bcc48
Vinicius Moraes <viniciusom01@hotmail.com>
2020-01-28 14:58:26 -0300

    Views com herança

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -8,12 +8,19 @@ class NegociacaoController {
         this._listaNegociacoes = new ListaNegociacoes();
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
     }
 
     adiciona(event) {
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem.texto = 'Negociação adicionada com sucesso!';
+        this._mensagemView.update(this._mensagem);
+
         this._limpaFormulario();
     }
 
--- js/app/views/NegociacoesView.js
+++ js/app/views/NegociacoesView.js
@@ -1,10 +1,9 @@
-class NegociacoesView {
-
+class NegociacoesView extends View {
     constructor(elemento){
-        this._elemento = elemento;
+        super(elemento);
     }
 
-    _template(model){
+    template(model){
 
         return `
         <table class="table table-hover table-bordered">
@@ -41,7 +40,4 @@ class NegociacoesView {
         `;
     }
 
-    update(model) {
-        this._elemento.innerHTML = this._template(model);
-    }
 }

2b2caf35ebe51c86160a3f5e6f320ecc3a2a87fa
Vinicius Moraes <viniciusom01@hotmail.com>
2020-02-28 16:24:33 -0300

    Adicionado Interceptação de dados

--- js/app/models/ListaNegociacoes.js
+++ js/app/models/ListaNegociacoes.js
@@ -1,15 +1,11 @@
 class ListaNegociacoes {
 
-    constructor(contexto, armadilha) {
+    constructor() {
         this._negociacoes = []
-        this._armadilha = armadilha;
-        this._contexto = contexto;
     }
 
     adiciona(negociacao) {
         this._negociacoes.push(negociacao);
-        // this._armadilha(this);
-        Reflect.apply(this._armadilha, this._contexto, [this]);
     }
 
     get negociacoes(){
@@ -17,9 +13,6 @@ class ListaNegociacoes {
     }
 
     esvazia() {
-
-        this._negociacoes = [];
-        // this._armadilha(this);
-        Reflect.apply(this._armadilha, this._contexto, [this]);
+        this._negociacoes = [];   
     }
 }
